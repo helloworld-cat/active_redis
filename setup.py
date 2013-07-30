d5b785bec3c329341fe079d4d3803c8d3f73e96e
Sam <samuel@pagedegeek.com>
2013-07-30 14:00:40 +0200

    edit setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
     name='active_redis',
     version='0.0.1',
     scripts=[],
-    packages=['active_redis']
+    install_requires = ['redis'],
+    packages=['active_redis'],
+    author = 'Samuel Sanchez',
+    author_email = 'samuel@pagedegeek.com',
+    description = 'Simple ActiveRecord pattern for Redis',
+    keywords = 'Redis ActiveRecord',
+    url = 'http://github.com/PagedeGeek/active_redis'
 )
 
